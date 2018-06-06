aea91ecc1d2831854fbec1b4d95791742919fd1c
liusong <liusong@douyu.tv>
2018-06-06 17:21:29 +0800

    fix(global): fix eslint

--- src/connect.ts
+++ src/connect.ts
@@ -2,7 +2,8 @@ import { inject, observer, IStoresToProps, IReactComponent } from 'mobx-react';
 import { selector } from './selector';
 import { withProps } from './withProps';
 
-export default function connect(dependencies: string[], mapDepsToProps: IStoresToProps<any>) {
-  return (Component: IReactComponent) =>
+const connect = (dependencies: string[], mapDepsToProps: IStoresToProps<any>) =>
+  (Component: IReactComponent) =>
     inject(selector(dependencies))(observer(withProps(mapDepsToProps)(Component)));
-}
+
+export default connect;
